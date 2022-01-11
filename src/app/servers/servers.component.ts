8bee90497fcad64c11ddcbfe257fb62bbad7d9df
Kevin Barreto <barretokevin@hotmail.com>
2022-01-10 19:48:30 -0600

    add event binding to input via (input) method

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,14 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css'],
+  styles: [
+    `
+      input {
+        width: 200px;
+      }
+    `,
+  ],
 })
 export class ServersComponent {
   allowNewServer: boolean = false;
+  serverCreation: string = 'No server was created';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onServerCreation() {
+    this.serverCreation = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
